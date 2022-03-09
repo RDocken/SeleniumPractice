f34c87dcf75d020c0d360ac86719549c2eda5192
RDocken <74928236+RDocken@users.noreply.github.com>
2022-03-08 19:19:24 -0600

    changed driver path

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -18,7 +18,7 @@ public class MainClass {
     public void ubuntuSetup(){
         System.out.println("Start of ubuntu setup");
 
-        System.setProperty("webdriver.chrome.driver", "/home/single/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver");
 
         //create ChromeOptions
         ChromeOptions options = new ChromeOptions();
