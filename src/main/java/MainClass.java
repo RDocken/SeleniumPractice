107a983a15e55b83856ef414085cfb92f5ebb3c3
RDocken <74928236+RDocken@users.noreply.github.com>
2022-03-13 16:08:30 -0500

    reverted changes to chromdriver path

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -18,7 +18,11 @@ public class MainClass {
     public void ubuntuSetup(){
         System.out.println("Start of ubuntu setup");
 
-        System.setProperty("webdriver.chrome.driver", "/home/jenkins/tools/chromedriver/chromedriver");
+        //this works on vm
+        System.setProperty("webdriver.chrome.driver", "/home/single/chromedriver");
+
+        //attempt on jenkins
+//        System.setProperty("webdriver.chrome.driver", "/home/jenkins/tools/chromedriver/chromedriver");
 
         //create ChromeOptions
         ChromeOptions options = new ChromeOptions();
